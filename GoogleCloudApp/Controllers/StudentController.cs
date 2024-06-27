172d87c88e49935983471eed8afdfbdba62c47e5
pusamsaikumar <https://github.com/pusamsaikumar>
2024-06-27 21:19:30 +0530

    controller added

---
+++ GoogleCloudApp/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleCloudApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public StudentController()
+        {
+            
+        }
+        [HttpGet]
+        [Route("GetStudentDetails")]
+        public IActionResult GetstudentDetails()
+        {
+            return Ok(new
+            {
+                Name="P. Sai Kumar",
+                AGE="28",
+                DateofBirth ="09-11-1994",
+                Marks="523",
+                Grades="A1"
+
+
+            });
+        }
+    }
+}
